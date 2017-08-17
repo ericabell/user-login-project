2966112a773e1530d9c62f3465924b413ca4deab
Eric Abell <ericabell@users.noreply.github.com>
2017-08-17 17:05:07 -0500

    Fixed password input type and ending it here for the day.
    I need to go back and figure out how to do auth without session
    and cookie data.

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ store.on('error', (e) => {
 
 // session data structure:
 // collection name is user and will hold three things
-// 1. this user's current status: logged in or logged out
-// 2. this user's username
-// 3. this user's password
+// 1. loggedin: true or false
+// 2. username: this user's username
+// 3. password: this user's password
 
 app.use(session({
   secret: 'keyboard cat',
@@ -43,6 +43,22 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
+// GENERAL CATCH FOR ALL ROUTES TO CHECK LOGIN STATUS
+
+app.use( (req, res, next) => {
+  console.log('catch all');
+  console.log(JSON.stringify(req.session.user));
+  if( req.session.user && req.session.user.loggedin ) {
+    res.render('index', {data: req.session.user})
+  } else {
+    // user needs to login
+    next();
+  }
+})
+
+app.get('/login', (req, res, next) => {
+  res.render('login')
+});
 
 app.get('/', (req, res, next) => {
   // if it doesn't exist, assume we have never seen this user before
@@ -66,40 +82,19 @@ app.get('/', (req, res, next) => {
 
 app.post('/', (req, res, next) => {
   // this is where a user has typed some credentials and we need to check them
-  res.send('need to check credentials' + req.body.username + req.body.password)
+  res.render('index', {data: req.session.user});
 
 })
 
-app.get('/login', (req, res, next) => {
-  res.render('login')
-});
+
 
 app.post('/login', (req, res, next) => {
   // this is where we will auth a user based on what they entered in the form
 
-  // they better have req.session.user
-  if( !req.session.user ) {
-    // error, we've never seen this person before
-    res.send('req.session.user should have existed, but was not')
-  } else {
-    // grab the info that they entered
-    let username = req.body.username;
-    let password = req.body.password;
-    console.log('authenticate user: ' + username + ' ' + password);
+  // user has no cookie => no session is available
+  // all we have is the database to authenticate against....
 
-    // check their info against req.user
-
-  }
-
-
-
-  // TODO need to check credentials
-  if( username != req.body.username || password != req.body.password ) {
-    // auth failed
-  } else {
-    // auth success! redirect to / as authenticated user
-    res.redirect('/');
-  }
+  res.send('need to find the user in the database. no cookie, no session')
 })
 
 app.post('/newaccount', (req, res, next) => {
