01f1d5d09e6cc2c0616ef508b0caecf771cf5cc7
Eric Abell <ericabell@users.noreply.github.com>
2017-08-17 11:19:57 -0500

    Set up store and check for whether we have seen the user or not

--- app.js
+++ app.js
@@ -4,10 +4,56 @@ const mongoDBStore = require('connect-mongodb-session')(session);
 const mustacheExpress = require('mustache-express');
 
 let app = express();
+let store = new mongoDBStore(
+  {
+    uri: 'mongodb://localhost:27017/user-login-project',
+    collection: 'user'
+  }
+);
+
+store.on('error', (e) => {
+  assert.ifError(e);
+  assert.ok(false);
+});
+
+
+// session data structure:
+// collection name is user and will hold three things
+// 1. this user's current status: logged in or logged out
+// 2. this user's username
+// 3. this user's password
+
+app.use(session({
+  secret: 'keyboard cat',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 //1 week
+  },
+  store: store,
+  resave: true,
+  saveUninitialized: true
+}));
 
 
 app.get('/', (req, res, next) => {
-  res.send('hello');
+  // try to get the data from req.session
+  let user = req.session.user;
+
+  // if it doesn't exist, assume we have never seen this user before
+  if (!user) {
+    console.log('We have never seen this user before.');
+    // create some data for the session associated with this user (cookie)
+    user = req.session.user = {username: '', password: '', loggedin: false};
+    res.send('never seen you before');
+  } else {
+    // we have seen this person before, but are they logged in?
+    console.log('We have seen this user before, need to check their status');
+    res.send('JSON.stringify(user)')
+  }
+
+});
+
+app.get('/login', (req, res, next) => {
+  res.send('this is the login page.')
 })
 
 app.listen(3000, () => {
